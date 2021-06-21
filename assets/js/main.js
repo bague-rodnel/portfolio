2217f1986f35d8f75b2e186f6ffd6963a44cb32e
Rodnel Bague <bague.rodnel@gmail.com>
2021-06-21 15:07:19 +0800

    final touches

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,7 +58,7 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('navbarDropdown');
-    // When we click on each nav__link, we remove the show class bootstrap uses
+    // When we click on each nav__link, we remove the .show class bootstrap uses
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
